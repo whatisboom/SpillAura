f542b7d32bf57ce9d58974494edc84fae8a74080
Brandon Jernigan <brandon.jernigan@flosports.tv>
2026-02-17 22:46:26 -0600

    init: add Xcode project files

---
+++ SpillAura/SpillAura/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpillAura
+//
+//  Created by Brandon Jernigan on 2/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpillAura/SpillAura/SpillAuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpillAuraApp.swift
+//  SpillAura
+//
+//  Created by Brandon Jernigan on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SpillAuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
